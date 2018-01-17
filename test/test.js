d9a585c6fac5935ffcc4086467df0e406933f7b1
jestersimpps <jestersimpps@gmail.com>
2018-01-17 16:51:24 +0100

    testing and travis

---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+
+describe('serializeJson json', () => {
+    it('should return a string', () => {
+        var result = index.Utils.serializeJson({ test: 'test' });
+        expect(result).to.equal('{"test":"test"}');
+    });
+});
+describe('deserializeJson string', () => {
+    it('should return an object', () => {
+        var result = index.Utils.deserializeJson('{"test":"test"}');
+        expect(result.test).to.equal('test');
+    });
+});
