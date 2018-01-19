3918185fecb06a9ffa3c38f8273bb57473bb1855
jestersimpps <jestersimpps@gmail.com>
2018-01-19 11:17:23 +0100

    fix tests

--- test/logging.js
+++ test/logging.js
@@ -1,48 +1,47 @@
 var expect = require('chai').expect;
 var index = require('../dist/index.js');
 
-
 describe('log hlf ', () => {
     it('should log debug messages', () => {
-        expect(index.Log.hlf.debug('test')).to.notThrowAnError;
+        index.Log.hlf.debug('test')
     });
     it('should log info messages', () => {
-        expect(index.Log.hlf.info('test')).to.notThrowAnError;
+        index.Log.hlf.info('test');
     });
     it('should log error messages', () => {
-        expect(index.Log.hlf.error('test')).to.notThrowAnError;
+        index.Log.hlf.error('test');
     });
 });
 describe('log grpc ', () => {
     it('should log debug messages', () => {
-        expect(index.Log.grpc.debug('test')).to.notThrowAnError;
+        index.Log.grpc.debug('test');
     });
     it('should log info messages', () => {
-        expect(index.Log.grpc.info('test')).to.notThrowAnError;
+        index.Log.grpc.info('test');
     });
     it('should log error messages', () => {
-        expect(index.Log.grpc.error('test')).to.notThrowAnError;
+        index.Log.grpc.error('test');
     });
 });
 describe('log pusher ', () => {
     it('should log debug messages', () => {
-        expect(index.Log.pusher.debug('test')).to.notThrowAnError;
+        index.Log.pusher.debug('test');
     });
     it('should log info messages', () => {
-        expect(index.Log.pusher.info('test')).to.notThrowAnError;
+        index.Log.pusher.info('test');
     });
     it('should log error messages', () => {
-        expect(index.Log.pusher.error('test')).to.notThrowAnError;
+        index.Log.pusher.error('test');
     });
 });
 describe('log sqs ', () => {
     it('should log debug messages', () => {
-        expect(index.Log.sqs.debug('test')).to.notThrowAnError;
+        index.Log.sqs.debug('test');
     });
     it('should log info messages', () => {
-        expect(index.Log.sqs.info('test')).to.notThrowAnError;
+        index.Log.sqs.info('test');
     });
     it('should log error messages', () => {
-        expect(index.Log.sqs.error('test')).to.notThrowAnError;
+        index.Log.sqs.error('test');
     });
 });
